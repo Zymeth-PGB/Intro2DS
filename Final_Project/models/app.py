4139a1435960b98da0182300910e1f14c80a3bc6
Zymeth-PGB <phangiabaovt2003@gmail.com>
2024-01-06 01:11:34 +0700

    Update 2: Deploy model

--- Final_Project/models/app.py
+++ Final_Project/models/app.py
@@ -1,29 +1,33 @@
 from flask import Flask, request, render_template
 import numpy as np
+import pandas as pd
 import pickle
 from src.feature_module.build_features import *
 
 app = Flask(__name__)
 
-model = pickle.load(open('D:\\University\\INTRO_2_DS\\Final_Project\\models\\model.pkl', 'rb'))
+model = pickle.load(open('./Final_Project/models/model.pkl', 'rb'))
+data = pd.read_csv('./Final_Project/data/processed/preproces_data.csv')
 
 @app.route('/')
 def Home():
     return render_template('index.html')
 
-@app.route("/predict", methods = ["POST"])
+@app.route("/result", methods = ["POST"])
 def predict():
     values =  request.form.values()
     features = []
     for i in values:
-        if i[0] >= '0' and i[0] <= 9:
+        if i[0] >= '0' and i[0] <= '9':
             features.append(np.array(float(i)))
         else:
-            features.append(convert(i))
+            features.append(convert(label_district(data), label_street(data), i))
+            
     features = np.array(features)
+    features = np.reshape(features, (1, 5))
     pred = model.predict(features)
     
-    return render_template('index.html', prediction_text = 'Giá: {}'.format(pred))
+    return render_template('result.html', prediction = 'Giá: {:.1f} (Triệu/tháng)'.format(float(pred)))
 
 if __name__ == "__main__":
     app.run(debug = True)
--- Final_Project/src/feature_module/build_features.py
+++ Final_Project/src/feature_module/build_features.py
@@ -1,5 +1,23 @@
-from sklearn.preprocessing import LabelEncoder
+def label_district(data):
+    dis = sorted(data['Quận'].unique())
+    lb_dis = {}
 
-def convert(value):
-    label_encoder = LabelEncoder()
-    return label_encoder.fit_transform(value)
+    for i, j in enumerate(dis):
+        lb_dis[j] = i
+        
+    return lb_dis
+
+def label_street(data):
+    street = sorted(data['Đường'].unique())
+    lb_street = {}
+
+    for i, j in enumerate(street):
+        lb_street[j] = i
+        
+    return lb_street
+
+def convert(label_district, label_street, val):
+    if val in label_district.keys():
+        return label_district[val]
+    else:
+        return label_street[val]
